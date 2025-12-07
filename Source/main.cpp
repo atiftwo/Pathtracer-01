1cd742434508bd86358797f8e377ac443023f446
AtifTwo <myrdcarcade@gmail.com>
2025-12-07 19:24:24 +0600

    Added build type differentiation for source code.

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 
-#ifndef BUILD_TYPE
-	#define BUILD_TYPE "Unconfigured"
+#ifdef Debug
+	#define BUILD_TYPE "DEBUG"
+#elif defined(Release)
+	#define BUILD_TYPE "RELEASE"
+#else
+	#define BUILD_TYPE "UNCONFIGURED"
 #endif
 
 int main()
 {
-	std::cout << "Hello, World!\nCurrent build configuration: " << BUILD_TYPE << std::endl;
+	std::cout << "Hello, World!\nCurrent build configuration is " << BUILD_TYPE << std::endl;
 	return 0;
 }
