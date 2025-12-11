9d25ba3fd9b76ed75ed6e8fb4c7042d4d7f25e74
AtifTwo <myrdcarcade@gmail.com>
2025-12-11 16:56:37 +0600

    Restore point

--- Source/Program.cpp
+++ Source/Program.cpp
@@ -2,7 +2,7 @@
 #include <glm/glm.hpp>
 
 #include <GLFW/glfw3.h>
-#include "../Program.hpp"
+#include "Program.hpp"
 
 #define GLFW_EXPOSE_NATIVE_WIN32
 #include <GLFW/glfw3native.h>
@@ -71,40 +71,13 @@ namespace Program
 
 		// TODO: Manually load dwmapi
 		{
-			enum DWMWINDOWATTRIBUTE {
-				DWMWA_NCRENDERING_ENABLED,
-				DWMWA_NCRENDERING_POLICY,
-				DWMWA_TRANSITIONS_FORCEDISABLED,
-				DWMWA_ALLOW_NCPAINT,
-				DWMWA_CAPTION_BUTTON_BOUNDS,
-				DWMWA_NONCLIENT_RTL_LAYOUT,
-				DWMWA_FORCE_ICONIC_REPRESENTATION,
-				DWMWA_FLIP3D_POLICY,
-				DWMWA_EXTENDED_FRAME_BOUNDS,
-				DWMWA_HAS_ICONIC_BITMAP,
-				DWMWA_DISALLOW_PEEK,
-				DWMWA_EXCLUDED_FROM_PEEK,
-				DWMWA_CLOAK,
-				DWMWA_CLOAKED,
-				DWMWA_FREEZE_REPRESENTATION,
-				DWMWA_PASSIVE_UPDATE_MODE,
-				DWMWA_USE_HOSTBACKDROPBRUSH,
-				DWMWA_USE_IMMERSIVE_DARK_MODE = 20,
-				DWMWA_WINDOW_CORNER_PREFERENCE = 33,
-				DWMWA_BORDER_COLOR,
-				DWMWA_CAPTION_COLOR,
-				DWMWA_TEXT_COLOR,
-				DWMWA_VISIBLE_FRAME_BORDER_THICKNESS,
-				DWMWA_SYSTEMBACKDROP_TYPE,
-				DWMWA_LAST
-			};
 			HMODULE dwmapiHandle = LoadLibrary("Dwmapi.dll");
 
 			HWND win32_windowHandle = glfwGetWin32Window(producedWindow);
 			int tmp = TRUE;
 
 			auto SetAttribute = (int(*)(HWND, DWORD, LPCVOID, DWORD))GetProcAddress(dwmapiHandle, "DwmSetWindowAttribute");
-			SetAttribute(win32_windowHandle, DWMWA_USE_IMMERSIVE_DARK_MODE, &tmp, sizeof(int));
+			SetAttribute(win32_windowHandle, 20, &tmp, sizeof(int));
 
 			FreeLibrary(dwmapiHandle);
 		}
