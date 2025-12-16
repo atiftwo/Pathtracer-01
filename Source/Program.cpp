16d67b55d0e608c81dce809de996b3a13325c2f6
AtifTwo <myrdcarcade@gmail.com>
2025-12-16 12:48:48 +0600

    ImGui implemented

--- Source/Program.cpp
+++ Source/Program.cpp
@@ -9,6 +9,10 @@
 #include <GLFW/glfw3native.h>
 #include <windows.h>
 
+#include <imgui.h>
+#include <backends/imgui_impl_glfw.h>
+#include <backends/imgui_impl_opengl3.h>
+
 // Callback functions' definition
 inline glm::ivec2 windowSize;
 inline void WindowResizeCallback(GLFWwindow*, int, int);
@@ -66,13 +70,36 @@ int Program::Window::Create() {
 		gladLoadGLLoader(loadProcFunc);
 	}
 	glfwSwapInterval(1);
+	IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+
+	// Setup Dear ImGui style
+	ImGui::StyleColorsDark();
+	//ImGui::StyleColorsLight();
+    float main_scale = ImGui_ImplGlfw_GetContentScaleForMonitor(glfwGetPrimaryMonitor()); // Valid on GLFW 3.3+ only
+	// Setup scaling
+	ImGuiStyle& style = ImGui::GetStyle();
+	style.ScaleAllSizes(main_scale);        // Bake a fixed style scale. (until we have a solution for dynamic style scaling, changing this requires resetting Style + calling this again)
+	style.FontScaleDpi = main_scale;        // Set initial font scale. (using io.ConfigDpiScaleFonts=true makes this unnecessary. We leave both here for documentation purpose)
+
+	// Setup Platform/Renderer backends
+	ImGui_ImplGlfw_InitForOpenGL(currentWindow, true);
+	#ifdef __EMSCRIPTEN__
+		ImGui_ImplGlfw_InstallEmscriptenCallbacks(window, "#canvas");
+	#endif
+	ImGui_ImplOpenGL3_Init("#version 330 core");
+    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
 	glfwShowWindow(currentWindow);
-	
 	return 0;
 }
 void Program::Window::Deinitialise() {
 	glfwTerminate();
 }
+
 void Program::Window::UpdateWindow() {
 	glfwSwapBuffers(currentWindow);
 	glfwPollEvents();
--- Source/main.cpp
+++ Source/main.cpp
@@ -1,50 +1,27 @@
 //#include <iostream>
 #include "Program.hpp"
+
 #include <glad/glad.h>
+#include <glm/glm.hpp>
+
+#include <imgui.h>
+#include <backends/imgui_impl_glfw.h>
+#include <backends/imgui_impl_opengl3.h>
 
 int main()
 {
 	Program::Window programWindow;
 	if (programWindow.Create() != 0) return 1;
-
-	// Shader initialisaiton
-	const char* fragmentShaderCode =
-	"#version 330 core\n"
-	"out vec4 FragColor;\n"
-	"void main() {FragColor = vec4(1.0f, 1.0f, 1.0f, 1.0f);}\0";
-
-	unsigned int fragmentShader;
-	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fragmentShader, 1, &fragmentShaderCode, NULL);
-	glCompileShader(fragmentShader);
-
-	const char* vertexShaderCode =
-	"#version 330 core\n"
-	"layout (location = 0) in vec3 aPos;\n"
-	"void main() {gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n}\0";
-
-	unsigned int vertexShader;
-	vertexShader = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(vertexShader, 1, &vertexShaderCode, NULL);
-	glCompileShader(vertexShader);
-
-	int shaderProgram = glCreateProgram();
-	glAttachShader(shaderProgram, vertexShader);
-	glAttachShader(shaderProgram, fragmentShader);
-	glLinkProgram(shaderProgram);
-
-	glDeleteShader(vertexShader);
-	glDeleteShader(fragmentShader);
 	
 	// Vertex	
 	unsigned int VAO;
 	glGenVertexArrays(1, &VAO);
 	glBindVertexArray(VAO);
 
-	float vertices[] = {
-		-0.5f, -0.5f, 0.0f,
-		0.5f, -0.5f, 0.0f,
-		0.0f, 0.5f, 0.0f
+	glm::vec3 vertices[] = {
+		{-0.5f, -0.5f, 0.0f},
+		{0.5f, -0.5f, 0.0f},
+		{0.0f, 0.5f, 0.0f}
 	};
 
 	unsigned int VBO;
@@ -55,16 +32,73 @@ int main()
 	glEnableVertexAttribArray(0);
 	
 	while (!programWindow.WindowWillClose()) {
-		glClearColor(0.8f, 0.8f, 0.8f, 1.0f);
+		//glClearColor(0.8f, 0.8f, 0.8f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
-		
+
+		// Rotate verts
+		auto RotateVertsFunc = [](glm::vec3& vert, float angle) {
+			float dst = sqrt(vert.x*vert.x + vert.y*vert.y);
+			float curAngle = atan(abs(vert.y/vert.x));
+
+			// Determine quadrant
+			if (vert.x > 0) {if (vert.y < 0) curAngle = 2*3.14159 - curAngle;} // 4th quadrant
+			else {
+				if (vert.y < 0) curAngle += 3.14159; // 3rd quadrant
+				else curAngle = 3.14159 - curAngle;
+			}
+
+			vert = {cos(angle+curAngle), sin(angle+curAngle), 0.0};
+		};
+
+		for (int i = 0; i < 3; ++i)
+		RotateVertsFunc(vertices[i], 0.0026*programWindow.GetElapsedTime());
+
 		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_DYNAMIC_DRAW);
 
-		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 
-		vertices[0] += 0.05;
+
+			ImGui_ImplOpenGL3_NewFrame();
+			ImGui_ImplGlfw_NewFrame();
+			ImGui::NewFrame();
+			// Create a window called "My First Tool", with a menu bar.
+			bool my_tool_active = true;
+			float my_color[4] = {0.5, 0.5, 0.8, 1.0};
+			ImGui::Begin("My First Tool", &my_tool_active, ImGuiWindowFlags_MenuBar);
+			if (my_tool_active) {
+				if (ImGui::BeginMenuBar())
+				{
+					if (ImGui::BeginMenu("File"))
+					{
+						if (ImGui::MenuItem("Open..", "Ctrl+O")) { /* Do stuff */ }
+						if (ImGui::MenuItem("Save", "Ctrl+S"))   { /* Do stuff */ }
+						if (ImGui::MenuItem("Close", "Ctrl+W"))  { my_tool_active = false; }
+						ImGui::EndMenu();
+					}
+					ImGui::EndMenuBar();
+				}
+
+				// Edit a color stored as 4 floats
+				ImGui::ColorEdit4("Color", my_color);
+
+				// Generate samples and plot them
+				float samples[100];
+				for (int n = 0; n < 100; n++)
+					samples[n] = sinf(n * 0.2f + ImGui::GetTime() * 1.5f);
+				ImGui::PlotLines("Samples", samples, 100);
+
+				// Display contents in a scrolling region
+				ImGui::TextColored(ImVec4(1,1,0,1), "Important Stuff");
+				ImGui::BeginChild("Scrolling");
+				for (int n = 0; n < 50; n++)
+					ImGui::Text("%04d: Some text", n);
+				ImGui::EndChild();
+			}
+			ImGui::End();
+
+		ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
 		programWindow.UpdateWindow();
 	}
